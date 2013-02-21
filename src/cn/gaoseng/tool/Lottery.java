d384d4f2c92a9cd1ccff344b8153847ee481e138
Howe Chiang <howechiang@gmail.com>
2013-02-21 17:21:45 +0800

    1

--- src/cn/gaoseng/tool/Lottery.java
+++ src/cn/gaoseng/tool/Lottery.java
@@ -1,48 +1,66 @@
 package cn.gaoseng.tool;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.nutz.dao.Dao;
-import org.nutz.dao.Sqls;
-import org.nutz.dao.sql.Sql;
-import org.nutz.dao.sql.SqlCallback;
-import org.nutz.ioc.loader.annotation.IocBean;
 import cn.gaoseng.bean.qiuqian.Qianwen;
-@IocBean(fields = {"dao"})
+
 public class Lottery {
-	private static Dao dao;
 
 	public static String getLottery(String typeId) {
 
 		String rtn = "";
-
-		Sql sql = Sqls.create("SELECT t.* FROM gs_qiuqian_qianwen t WHERE t.TYPEID=@TYPEID");
-		//通过typeid取gs_qiuqian_qianwen的id 
-		sql.params().set("TYPEID",typeId);
-		sql.setCallback(new SqlCallback() {
-			@Override
-			public Object invoke(Connection conn, ResultSet rs, Sql sql) throws SQLException {
-				List<Qianwen> list = new ArrayList<Qianwen>();
-				while (rs.next()){
-					list.add(new Qianwen(Integer.parseInt(rs.getString("id"))));		
-				}
-				return list;
-			}
-			
-		});
-		
-		dao.execute(sql);
-		List<Double> orignalRates = new ArrayList<Double>(sql.getList(Qianwen.class).size());
-
-		for (Qianwen qw : sql.getList(Qianwen.class)) {
-			double probability = 1.0/sql.getList(Qianwen.class).size();
+		List<Qianwen> qws = new ArrayList<Qianwen>();
+		if (typeId.equals("1")) {
+			qws.add(new Qianwen(1));
+			qws.add(new Qianwen(2));
+			qws.add(new Qianwen(3));
+			qws.add(new Qianwen(4));
+			qws.add(new Qianwen(5));
+			qws.add(new Qianwen(6));
+			qws.add(new Qianwen(7));
+			qws.add(new Qianwen(8));
+			qws.add(new Qianwen(9));
+			qws.add(new Qianwen(10));
+			qws.add(new Qianwen(11));
+			qws.add(new Qianwen(12));
+			qws.add(new Qianwen(13));
+			qws.add(new Qianwen(14));
+			qws.add(new Qianwen(15));
+			qws.add(new Qianwen(16));
+			qws.add(new Qianwen(17));
+			qws.add(new Qianwen(18));
+			qws.add(new Qianwen(19));
+			qws.add(new Qianwen(20));
+			qws.add(new Qianwen(21));
+			qws.add(new Qianwen(22));
+			qws.add(new Qianwen(23));
+			qws.add(new Qianwen(24));
+			qws.add(new Qianwen(25));
+			qws.add(new Qianwen(26));
+			qws.add(new Qianwen(27));
+			qws.add(new Qianwen(28));
+			qws.add(new Qianwen(29));
+			qws.add(new Qianwen(30));
+			qws.add(new Qianwen(31));
+			qws.add(new Qianwen(32));
+			qws.add(new Qianwen(33));
+			qws.add(new Qianwen(34));
+			qws.add(new Qianwen(35));
+			qws.add(new Qianwen(36));
+			qws.add(new Qianwen(37));
+			qws.add(new Qianwen(38));
+			qws.add(new Qianwen(39));
+			qws.add(new Qianwen(40));
+		} else {
+			qws.add(new Qianwen(0));
+		}
+		List<Double> orignalRates = new ArrayList<Double>(qws.size());
+		for (Qianwen qw : qws) {
+			double probability = 1.0 / qws.size();
 			if (probability < 0) {
 				probability = 0;
 			}
@@ -59,9 +77,8 @@ public class Lottery {
 		}
 
 		for (Entry<Integer, Integer> entry : count.entrySet()) {
-			rtn = sql.getList(Qianwen.class).get(entry.getKey()).toString();
+			rtn = qws.get(entry.getKey()).toString();
 		}
 		return rtn;
 	}
-
 }
